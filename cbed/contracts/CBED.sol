34a533cfc6e8189025dc6212f92aabdc54af3d5b
Andy <hxdid@yahoo.com>
2018-03-01 17:13:20 -0500

    updated CBEDAdmin.sol to add modifier and update onlyOwner functions:
    modifier onlyAdmin(string _adminId) ;
    
    function setCBED(string _adminId,string _cid, string _fullname,string _coursename,string _issuedOn,string _validUntil ) onlyAdmin( _adminId) onlyAdminPermitStatusON( _adminId) public;

--- cbed/contracts/CBED.sol
+++ cbed/contracts/CBED.sol
@@ -1,56 +1,69 @@
-/*.
+/*
 This CBED Contract implements the standard functionality for recording the records of Certified Blockchain Ethereum Developers in a Blockchain.
-.*/
+*/
 
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.18;
 
 
 /* The contract of CBED */
 
-contract CBED 
-{
+contract CBED {
 
-	/* The CBED Struct */
+	/* owner address */
+    address public owner;
+
+
+    /* set modifier for onlyOwner */
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+
+    /* function transfer to new owner for onlyOwner */
+    function transferOwnership(address newOwner) onlyOwner public {
+        owner = newOwner;
+    }
+
+
+	/* function destroy for onlyowner to stop and pause the chain functions */
+	function destroy() onlyOwner public {
+		selfdestruct(owner);
+	}
+
+
+	/* the CBED struct defines CBED elements */
 	struct CBEDStruct {
 		uint256 id;  		//certificaton index id
 		string cid;  		//certificaton id
 		string fullname;	//Certified Blockchain Ethereum Developer's name
 		string coursename;	//certified Blockchain course name 
-		string issuedOn;	//certificaton issued date i.e. 20180228
-		string validUntil;	//certificaton expire date i.e. 20280227
+		string issuedOn;	//certificaton issued date, i.e. 20180228
+		string validUntil;	//certificaton expire date, i.e. 20280227
 	} 
 
-	CBEDStruct[] public CBEDStructArray;
-	CBEDStruct public CED;
 
-	uint256 public CEDID =0 ;
+	/* declaration of CBED to hold an instance of CBED*/
+	CBEDStruct public CED; 
+
+	/* declaration of CBED array to store CBED instances*/
+	CBEDStruct[] public CBEDStructArray; 
+
+	/* initial CBEDID for CBED */
+	uint256 public CEDID = 0 ;
 
 	/* Define variable cert of the type string */
 	string cert;
 
+
 	/* This runs when the contract is executed */
 	function CBED() public {
-		owner = msg.sender;
-		cert = "Certificed Ethereum Program";
+		owner = msg.sender;  //set owner
+		cert = "Certificed Ethereum Program";  //set cert value
 	}
 
 
-
-	/* function for admin only */	
-	function setCBED(string _cid, string _fullname,string _coursename,string _issuedOn,string _validUntil ) public {
-	//?	if (msg.sender == isAdmin)  must check isAdmin first
-		    CEDID += 1;
-		    CED.id = CEDID;
-		    CED.cid = _cid;
-		    CED.fullname = _fullname;
-		    CED.coursename = _coursename;
-		    CED.issuedOn = _issuedOn;
-		    CED.validUntil = _validUntil;
-		    
-		    CBEDStructArray.push(CED) -1;
-	}
-
-
+    /* function getCBEDStructByID() for getting a cert by index id */
 	function getCBEDStructByID(uint256 i) constant public returns (uint256 , string , string ,string ,string ,string ) {
 	  if (i>0) {i = i-1;} 
 	    else { i =0 ;}
@@ -58,6 +71,7 @@ contract CBED
 	}
 
 
+    /* function getCBEDStructByCID() for getting a cert by certified developer id */
 	function getCBEDStructByCID(string _cid) constant public returns (uint256 , string , string ,string ,string ,string ) {
 	  for (uint256 i = 0; i < CBEDStructArray.length ; i++) {
 	      if ( keccak256(_cid)==keccak256(CBEDStructArray[i].cid) ) {
@@ -67,104 +81,15 @@ contract CBED
 	}
 
 
+    /* function countCBEDStruct() for calculating certified developer numbers in the CBED blockchain */
 	function countCBEDStruct() view public returns (uint256) {
 	    return CBEDStructArray.length;
 	}
 
 
-	function getlastCED( ) constant public returns (uint256 _id, string _cid, string _fullname,string _coursename,string _issuedOn,string _validUntil) {
+    /* function getlastCBED() for getting the last certified developer cert in the CBED blockchain */
+	function getlastCBED( ) constant public returns (uint256 _id, string _cid, string _fullname,string _coursename,string _issuedOn,string _validUntil) {
 		return (CED.id, CED.cid,CED.fullname,CED.coursename,CED.issuedOn, CED.validUntil );
-
-	}
-
-
-
-	/* function destroy() for owner only to stop, pause the chain functions */
-	function destroy() public {
-
-		if (msg.sender == owner) 
-			selfdestruct(owner);
-
-	}
-
-
-
-
-	/* CBED Administrator */
-
-	/* The CBED Admin Struct */
-	struct CBEDAdminStruct {
-		uint256 adminId;  		//Admin index id
-		string fullname;		//Admin name
-		string lastLogin;		//last login time 
-		string adminPermitStatus;	//Admin issueing cert permission activation ON/OFF
-	} 
-
-	CBEDAdminStruct[] public CBEDAdminStructArray;
-	CBEDAdminStruct public CBEDAdmin;
-
-	uint256 public CBEDAdminID =0 ;
-
-
-	/* function addCBEDAdmin() for owner only */
-	function addCBEDAdmin(string _adminFullname,string _adminPermitStatus ) public {
-		if (msg.sender == owner) 
-		    CBEDAdminID += 1;
-		    CBEDAdmin.adminId = CBEDAdminID;
-		    CBEDAdmin.fullname = _adminFullname;
-		    CBEDAdmin.adminPermitStatus = _adminPermitStatus;
-		    
-		    CBEDAdminStructArray.push(CBEDAdmin) -1;
-	}
-
-
-	/* function activeCBEDAdminByAdminID() for owner only */
-	function activeCBEDAdminByAdminID(string _adminId ) public {
-		if (msg.sender == owner) 
-
-		    //find admin and set adminPermitStatus = ON
-		    CBEDAdminStructArray.push(CBEDAdmin) -1;
-	}
-
-
-	/* function deactiveCBEDAdminByAdminID() for owner only */
-	function deactiveCBEDAdminByAdminID(string _adminId ) public {
-		if (msg.sender == owner) 
-
-		    //find admin and set adminPermitStatus = OFF
-		    CBEDAdminStructArray.push(CBEDAdmin) -1;
-	}
-
-
-	/* function deleteCBEDAdminByAdminID() for owner only */
-	function deleteCBEDAdminByAdminID(string _adminId ) public {
-		if (msg.sender == owner) 
-
-		    //find admin and delete it
-		    CBEDAdminStructArray.pull(CBEDAdmin) -1;
-	}
-
-
-	/* function searchCBEDAdminByAdminID() for owner only */
-	function searchCBEDAdminByAdminID(string _adminId ) public {
-		if (msg.sender == owner) 
-
-		//find admin by admin id
-		if (i>0) {i = i-1;} 
-		else { i =0 ;}
-		return (CBEDAdminStructArray[i].adminId,CBEDAdminStructArray[i].fullname,CBEDAdminStructArray[i].lastLogin,CBEDAdminStructArray[i].adminPermitStatus);
-
-	}
-
-
-	/* function listCBEDAdmin() for owner only */
-	function listCBEDAdmin() public {
-		if (msg.sender == owner) 
-
-		//find admin 
-		if (i>0) {i = i-1;} 
-		else { i =0 ;}
-		return (CBEDAdminStructArray[i].id, CBEDAdminStructArray[i].cid,CBEDAdminStructArray[i].fullname,CBEDAdminStructArray[i].coursename,CBEDAdminStructArray[i].adminPermitStatus);
 
 	}
 
--- cbed/contracts/CBEDAdmin.sol
+++ cbed/contracts/CBEDAdmin.sol
@@ -1,68 +1,146 @@
+/*
+This CBED Admin Contract inhireted from CBED and implements the standard functionality for managing CBED Admins in the CBED Blockchain.
+*/
+
+
 import "./CBED.sol";
 
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.18;
 
+
+/* The contract of CBEDAdmin */
 
 contract CBEDAdmin is CBED {
 
-  struct CBEDAdminStruct {
-    uint256 id;
-    string cid;
-    string fullname;
-    string coursename;
-    string issuedOn;
-    string validUntil;
-  } 
+    /* the CBED admin struct */
+    struct cEDAdminStruct {
+      uint256 adminId;          //Admin index id
+      string fullname;          //Admin name
+      string lastLogin;         //last login time 
+      bool adminPermitStatus; //permission activation for Admin issueing cert to balockchain developer 
+    } 
 
-    CBEDAdminStruct[] public CEDStructArray;
-    CBEDAdminStruct public CED;
 
-    uint256 public CEDID =0 ;
+    /* declaration of CBED admin to hold an instance of CBED admin*/
+    cEDAdminStruct public cEDAdmin;
 
-  /* Define variable cert of the type string */
-  string cert;
+    /* declaration of CBED admin array to store CBED admin instances*/    
+    cEDAdminStruct[] public cEDAdminStructArray;
 
-  /* This runs when the contract is executed */
-  function CBEDAdmin() public {
-    cert = "Certificed Ethereum Program";
-  }
+    /* initial CBED admin ID for CBED admin*/
+    uint256 public cEDAdminID =0 ;
 
-  /* Main function */
-  // "CBED20180201","Sean Xun Cao","Certified Blockchain Ethereum Developer","20180218","20280218"
-  function setCED(string _cid, string _fullname,string _coursename,string _issuedOn,string _validUntil ) public {
-        CEDID += 1;
-        CED.id = CEDID;
-        CED.cid = _cid;
-        CED.fullname = _fullname;
-        CED.coursename = _coursename;
-        CED.issuedOn = _issuedOn;
-        CED.validUntil = _validUntil;
+
+    /* CBED Administration */
+
+    /* function addcEDAdmin() for owner only */
+    function addcEDAdmin(string _adminFullname,bool _adminPermitStatus ) onlyOwner public {
+        cEDAdminID += 1;
+        cEDAdmin.adminId = cEDAdminID;
+        cEDAdmin.fullname = _adminFullname;
+        cEDAdmin.adminPermitStatus = _adminPermitStatus;  //default is true          
+        cEDAdminStructArray.push(cEDAdmin) -1;
+    }
+
+
+    /* function activecEDAdminByAdminID() for owner only */
+    function activecEDAdminByAdminID(string _adminId ) onlyOwner public {
         
-        CEDStructArray.push(CED) -1;
-    
-  }
-
-    function getCEDStructByID(uint256 i) constant public returns (uint256 , string , string ,string ,string ,string ) {
-      if (i>0) {i = i-1;} 
-        else { i =0 ;}
-      return (CEDStructArray[i].id, CEDStructArray[i].cid,CEDStructArray[i].fullname,CEDStructArray[i].coursename,CEDStructArray[i].issuedOn, CEDStructArray[i].validUntil);
-    }
- 
-
-    function getCEDStructsByCID(string _cid) constant public returns (uint256 , string , string ,string ,string ,string ) {
-      for (uint256 i = 0; i < CEDStructArray.length ; i++) {
-          if ( keccak256(_cid)==keccak256(CEDStructArray[i].cid) ) {
-            return (CEDStructArray[i].id, CEDStructArray[i].cid,CEDStructArray[i].fullname,CEDStructArray[i].coursename,CEDStructArray[i].issuedOn, CEDStructArray[i].validUntil);
-          }
-      }    
+      //find admin and set adminPermitStatus = true for the admin (will add steps for active)
+      bool isAdminPermitStatus = true;
+      _adminId ="";
+      
     }
 
 
-    function countCEDStruct() view public returns (uint256) {
-        return CEDStructArray.length;
+    /* function deactivecEDAdminByAdminID() for owner only */
+    function deactivecEDAdminByAdminID(string _adminId ) onlyOwner public {
+        
+      //find admin and set adminPermitStatus = false for the admin (will add steps for deactive)
+      bool isAdminPermitStatus = false;      
+      _adminId ="";     
+      
     }
 
-  function getlastCED( ) constant public returns (uint256 _id, string _cid, string _fullname,string _coursename,string _issuedOn,string _validUntil) {
-    return (CED.id, CED.cid,CED.fullname,CED.coursename,CED.issuedOn, CED.validUntil );
-  }
+
+    /* function deletecEDAdminByAdminID() for owner only */
+    function deletecEDAdminByAdminID(string _adminId ) onlyOwner public {
+        
+      //find admin and delete it (will add steps for searching and deleting)
+      _adminId ="";      
+
+    }
+
+
+    /* function searchcEDAdminByAdminID()  */
+    function searchcEDAdminByAdminID(string _adminId ) constant public returns ( string ) {
+        
+      //find admin by admin id and return the admin (will add steps for searching)
+      _adminId ="";      
+      return ;
+    }
+
+
+    /* function listcEDAdmin()  */
+    function listcEDAdmin()  constant public returns ( string ) { 
+        
+      //find admin list and return all admins  (will add steps for listing)
+
+      return ;    
+
+      /*if (uint256 i>0) {i = i-1;} //example for find and return cbed admin list
+      else { i =0 ;}
+      return (cEDAdminStructArray[i].id, cEDAdminStructArray[i].cid,cEDAdminStructArray[i].fullname,cEDAdminStructArray[i].coursename,cEDAdminStructArray[i].adminPermitStatus);
+      */
+    }
+
+
+    /* set modifier for onlyAdmin */
+    modifier onlyAdmin(string _adminId)  {
+
+      //find admin by admin id and return the admin (will add steps for searching)
+      //if find set isAdmin=true, elst set isAdmin=false
+      bool isAdmin = true;
+      require(isAdmin);
+      return ;
+      _;
+    }
+
+
+    /* set modifier for onlyAdminPermitStatusON */
+    modifier onlyAdminPermitStatusON(string _adminId)  {
+
+      //find admin by admin id and return the admin permit status (will add steps for searching)
+      //if ON set isAdminPermitStatus=true, elst set isAdminPermitStatus=false
+      bool isAdminPermitStatus = true;
+      require(isAdminPermitStatus);
+      return ;
+      _;
+    }
+
+
+    //we may not need login function now but keep as comments here for reference
+    /* function CBEDAdminLogin() will be for admin only  
+    function CBEDAdminLogin(string _adminId,string _fullname) onlyAdmin( _adminId) public {
+    ////still have some thing need to be discussed about login function ???? 
+    ////maybe we don't need login function but need check onlyAdmin for each admin function. ???
+      return ;
+    }*/
+
+
+    /* function setCBED() will be for admin only  */
+    function setCBED(string _adminId,string _cid, string _fullname,string _coursename,string _issuedOn,string _validUntil ) onlyAdmin( _adminId) onlyAdminPermitStatusON( _adminId) public {
+    //must check if (isAdmin) 
+    //must check if (onlyAdminPermitStatus=ON)
+      CEDID += 1;
+      CED.id = CEDID;
+      CED.cid = _cid;
+      CED.fullname = _fullname;
+      CED.coursename = _coursename;
+      CED.issuedOn = _issuedOn;
+      CED.validUntil = _validUntil;      
+      CBEDStructArray.push(CED) -1;
+    }
+  
+
 }
