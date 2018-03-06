e177511d9942bf7abd042d36f8815a1c4dc6ba33
Andy <hxdid@yahoo.com>
2018-03-05 19:25:21 -0500

    implemented modifier for onlyAdminPermitStatusON

--- cbed/contracts/CBEDAdmin.sol
+++ cbed/contracts/CBEDAdmin.sol
@@ -39,53 +39,38 @@ contract CBEDAdmin is CBED {
 
     /* set modifier for onlyAdmin */
     modifier onlyAdmin(address _adminAddress,string _adminPassword)  {
-      //find admin by admin id and return the admin 
-      //if find set isAdmin=true, elst set isAdmin=false
-        bool isAdmin = false;
-        for (uint256 i = 0; i < cEDAdminStructArray.length ; i++) {
-        //  if ( keccak256(_adminAddress)==keccak256(cEDAdminStructArray[i].adminAddress) ) {
+      //if find the admin set isAdmin=true, else isAdmin=false
+      bool isAdmin = false;
+      for (uint256 i = 0; i < cEDAdminStructArray.length ; i++) {
           if ( _adminAddress==cEDAdminStructArray[i].adminAddress ) {             
-            if ( keccak256(cEDAdminStructArray[i].adminPassword) == keccak256(_adminPassword) )           
-            isAdmin = true;
-        }
-    }
-        require(isAdmin);
-        _;
-
-    /*function isTokenTransferOK(address currentOwner, address newOwner) public view returns (bool ok)
-    { // Check some arbitrary condition.
-      address tokenAddress = msg.sender;
-      return (keccak256(newOwner) & 0xff) == (bytes20(tokenAddress) & 0xff);
-    }*/
-    }
-
-
-    /* set modifier for onlyAdminPermitStatusON */
-    modifier onlyAdminPermitStatusON(string _adminId)  {
-
-      //find admin by admin id and return the admin permit status (will add steps for searching)
-      //if ON set isAdminPermitStatus=true, elst set isAdminPermitStatus=false
-      bool isAdminPermitStatus = true;
-      require(isAdminPermitStatus);
-      //return ;
+              if (keccak256(cEDAdminStructArray[i].adminPassword)==keccak256(_adminPassword)) {
+                isAdmin = true;
+              }             
+          }
+      }
+      require(isAdmin);
       _;
     }
 
 
-    //we may not need login function now but keep as comments here for reference
-    /* function CBEDAdminLogin() will be for admin only  
-    function CBEDAdminLogin(string _adminId,string _fullname) onlyAdmin( _adminId) public {
-    ////still have some thing need to be discussed about login function ???? 
-    ////maybe we don't need login function but need check onlyAdmin for each admin function. ???
-      return ; 
-    }*/
+    /* set modifier for onlyAdminPermitStatusON */
+    modifier onlyAdminPermitStatusON(address _adminAddress,string _adminPassword)  {
+      //find admin set isAdminPermitStatus = true else false
+      bool isAdminPermitStatus = false;
+      for (uint256 i = 0; i < cEDAdminStructArray.length ; i++) {
+          if ( _adminAddress==cEDAdminStructArray[i].adminAddress ) {             
+              if (keccak256(cEDAdminStructArray[i].adminPassword)==keccak256(_adminPassword)) {
+                isAdminPermitStatus = true;
+              }             
+          }
+      }      
+      require(isAdminPermitStatus);
+      _;
+    }
 
 
     /* function setCBED() will be for admin only  */
-    function setCBED(string _adminId,string _cid, string _fullname,string _coursename,string _issuedOn,string _validUntil,string _adminPassword ) onlyAdmin(msg.sender, _adminPassword)  public {
-    //must check if (isAdmin) 
-    //must check if (onlyAdminPermitStatus=true)
-
+    function setCBED(string _cid, string _fullname,string _coursename,string _issuedOn,string _validUntil,string _adminPassword) onlyAdmin(msg.sender, _adminPassword)  public {
       CEDID += 1;
       CED.id = CEDID;
       CED.cid = _cid;
@@ -96,8 +81,6 @@ contract CBEDAdmin is CBED {
       CBEDStructArray.push(CED) -1;
     }
   
-
-
 
     /* function addcEDAdmin() for owner only */
     function addcEDAdmin(string _adminFullname,bool _adminPermitStatus,address _adminAddress,string _adminPassword ) onlyOwner public {
@@ -113,8 +96,7 @@ contract CBEDAdmin is CBED {
 
 
     /* function activecEDAdminByAdminID() for owner only */
-    function activecEDAdminByAdminID(string _adminId ) onlyOwner public {
-        
+    function activecEDAdminByAdminID(string _adminId ) onlyOwner public {        
       //find admin and set adminPermitStatus = true for the admin (will add steps for active)
       bool isAdminPermitStatus = true;
       _adminId ="";
@@ -123,8 +105,7 @@ contract CBEDAdmin is CBED {
 
 
     /* function deactivecEDAdminByAdminID() for owner only */
-    function deactivecEDAdminByAdminID(string _adminId ) onlyOwner public {
-        
+    function deactivecEDAdminByAdminID(string _adminId ) onlyOwner public {        
       //find admin and set adminPermitStatus = false for the admin (will add steps for deactive)
       bool isAdminPermitStatus = false;      
       _adminId ="";     
@@ -133,8 +114,7 @@ contract CBEDAdmin is CBED {
 
 
     /* function deletecEDAdminByAdminID() for owner only */
-    function deletecEDAdminByAdminID(string _adminId ) onlyOwner public {
-        
+    function deletecEDAdminByAdminID(string _adminId ) onlyOwner public {        
       //find admin and delete it (will add steps for searching and deleting)
       _adminId ="";      
 
@@ -142,43 +122,36 @@ contract CBEDAdmin is CBED {
 
 
     /* function resetcEDAdminPasswordByAdminAddress() for owner only */
-    function resetcEDAdminPasswordByAdminAddress(address _adminAddress,string _adminPassword ) onlyOwner public {
-      //find admin and reset password for it 
-      //_adminAddress = 0x87219ad8E841dC02098be06A7abE0f37CA12670c;
+    function resetcEDAdminPasswordByAdminAddress(address _adminAddress,string _adminPassword ) onlyOwner public returns ( bool ) {
+        //find admin and reset password for it 
+        bool isReset = false;
         for (uint256 i = 0; i < cEDAdminStructArray.length ; i++) {
-          if ( keccak256(_adminAddress)==keccak256(cEDAdminStructArray[i].adminAddress) ) {
-            //return (cEDAdminStructArray[i].adminId, cEDAdminStructArray[i].fullname,cEDAdminStructArray[i].lastLogin,cEDAdminStructArray[i].adminPermitStatus,cEDAdminStructArray[i].adminAddress);
+          if ( _adminAddress==cEDAdminStructArray[i].adminAddress ) {
             cEDAdminStructArray[i].adminPassword = _adminPassword;
-
+            isReset = true;
+          }
         }
-    }
+        return isReset;        
     }
 
 
-  /* function searchcEDAdminByAdminID()  */
-  function searchcEDAdminByAdminID(uint _adminId) view public returns (uint256,string,string,bool,address) {
-    //find admin by admin id and return the admin 
-      uint i = _adminId;
-    if (i>0) {i = i-1;}
-      else { i = 0;}
+    /* function searchcEDAdminByAdminID()  */
+    function searchcEDAdminByAdminID(uint _adminId) view public returns (uint256,string,string,bool,address) {
+        //find admin by admin id and return the admin 
+        uint i = _adminId;
+        if (i>0) {i = i-1;}
+        else { i = 0;}
         return (cEDAdminStructArray[i].adminId,cEDAdminStructArray[i].fullname,cEDAdminStructArray[i].lastLogin,cEDAdminStructArray[i].adminPermitStatus,cEDAdminStructArray[i].adminAddress);
-
-  }
+    }
 
 
     /* function listcEDAdmin()  */
     function listcEDAdmin()  view public returns (uint256,string,string,bool,address) { 
       //find admin list and return all admins  
       for (uint256 i = 0; i < cEDAdminStructArray.length ; i++) {
-          //if ( keccak256(_cid)==keccak256(cEDAdminStructArray[i].cid) ) {
-            return (cEDAdminStructArray[i].adminId,cEDAdminStructArray[i].fullname,cEDAdminStructArray[i].lastLogin,cEDAdminStructArray[i].adminPermitStatus,cEDAdminStructArray[i].adminAddress);
-          //}
-       }    
-      
-
+          return (cEDAdminStructArray[i].adminId,cEDAdminStructArray[i].fullname,cEDAdminStructArray[i].lastLogin,cEDAdminStructArray[i].adminPermitStatus,cEDAdminStructArray[i].adminAddress);
+       }  
     }
-
-
 
 
 }
