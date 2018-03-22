0abab48b91b85d1440a15333f2c68ba3e69f2354
alistairzhu <alistairzhu@gmail.com>
2018-03-21 21:26:43 -0400

    Update WonderEgg.sol
    Add functions from ConvertLib.sol to send coin to specific account before start generate eggs.

--- cryptofrog/contracts/WonderEgg.sol
+++ cryptofrog/contracts/WonderEgg.sol
@@ -1,23 +1,26 @@
 pragma solidity ^0.4.18;
 
 import "./WonderToken.sol";
+import "./ConvertLib.sol"; //Zhu
 
 contract WonderEgg is WonderToken{
-    
+
   struct WonderStruct {
     address addr;
     uint256 id;
     uint256 wprice;
     bytes32 idName;
     uint256 Description;
-  } 
+  }
 
     WonderStruct[] public WonderStructArray;
     WonderStruct public wonder;
 
     mapping (address => WonderStruct) wonders;
+    mapping (address => uint) balances; // Zhu
+
     address[] public wonderAddressArray;
-    
+
     uint256 public wonderID =0 ;
 
     event wonderInfo(
@@ -27,11 +30,22 @@ contract WonderEgg is WonderToken{
         bytes32 idName,
         uint256 Description
     ) ;
-    
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value); // Zhu
+
+    function sendCoin(address receiver, uint amount) public returns(bool sufficient) {  //Zhu
+      if (balances[msg.sender] < amount) return false;
+      balances[msg.sender] -= amount;
+      balances[receiver] += amount;
+      Transfer(msg.sender, receiver, amount);
+      return true;
+    }
+
     function WonderEgg() WonderToken(21000000,"WonderCoin","WDR") payable public {
         owner = msg.sender;
+        balances[tx.origin] = 10000;  // Zhu
     }
-    
+
     function setWonderStruct(address _address, uint256 _wprice, bytes32 _idName, uint256 _Description) public {
 
         wonder = wonders[_address];
@@ -41,16 +55,16 @@ contract WonderEgg is WonderToken{
         wonder.wprice = _wprice;
         wonder.idName = _idName;
         wonder.Description = _Description;
-        
+
         wonderAddressArray.push(_address) -1;
-        
+
         WonderStructArray.push(wonder) -1;
         wonderInfo(_address,wonderID, _wprice, _idName, _Description) ;
-        
+
     }
 
     function getWonderStructs() view public returns (address[]) {
-            return (wonderAddressArray); 
+            return (wonderAddressArray);
     }
 
     function getWonderStructByID(uint256 i) view public returns (address, uint256 , uint256 , bytes32 , uint256) {
@@ -62,7 +76,7 @@ contract WonderEgg is WonderToken{
         if (ins == WonderStructArray[i].addr){
             return (WonderStructArray[i].addr,WonderStructArray[i].id, WonderStructArray[i].wprice, WonderStructArray[i].idName, WonderStructArray[i].Description);
         }
-        
+
     }
 
 
@@ -73,5 +87,5 @@ contract WonderEgg is WonderToken{
     function countWonderStructs() view public returns (uint256) {
         return wonderAddressArray.length;
     }
-    
+
 }
