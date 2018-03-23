191f1f3c7865732fe2ee00651b43539085903121
alistairzhu <alistairzhu@gmail.com>
2018-03-22 22:41:24 -0400

    Update my.js
    Add function to pay fee for generate WonderEggs.

--- cryptofrog/app/my/my.js
+++ cryptofrog/app/my/my.js
@@ -5,8 +5,15 @@ import { default as contract } from 'truffle-contract'
 // Import our contract artifacts and turn them into usable abstractions.
 import WonderEgg_artifacts from '../../build/contracts/WonderEgg.json'
 
+// Import our contract artifacts and turn them into usable abstractions. // Zhu
+import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+
 // WonderEgg is our usable abstraction, which we'll use through the code below.
 var WonderEgg = contract(WonderEgg_artifacts);
+
+// MetaCoin is our usable abstraction, which we'll use through the code below.  // Zhu
+var MetaCoin = contract(metacoin_artifacts);
+
 
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
@@ -17,9 +24,9 @@ var account;
 var sellrate =1;
 var buyrate =1;
 
-var wondereggContract ;
-var weggs ;
-            
+        var wondereggContract ;
+        var Coursetro ;
+
 
 window.App = {
   start: function() {
@@ -27,6 +34,9 @@ window.App = {
 
     // Bootstrap the WonderEgg abstraction for Use.
     WonderEgg.setProvider(web3.currentProvider);
+
+    // Bootstrap the MetaCoin abstraction for Use. // Zhu
+    MetaCoin.setProvider(web3.currentProvider);
 
     // Get the initial account  and set defaultAccount
     web3.eth.getAccounts(function(err, accs) {
@@ -58,10 +68,10 @@ window.App = {
   refreshBalance: function() {
     var self = this;
     $("#loader").show();
-    var wegga;
+    var cert;
     WonderEgg.deployed().then(function(instance) {
-      wegga = instance;
-      return wegga.getPrices();
+      cert = instance;
+      return cert.getPrices();
     }).then(function(result){   // sellrate,buyrate
         if(result)
             {
@@ -83,8 +93,8 @@ window.App = {
 
     var outtext;
     WonderEgg.deployed().then(function(instance) {
-      wegga = instance;
-      return wegga.getlastWonderStruct({from: account});
+      cert = instance;
+      return cert.getlastWonderStruct({from: account});
     }).then(function(result){   // wonder.addr,wonder.id,wonder.wprice,wonder.idName,wonder.Description
         var text = 'test';
         if(result)
@@ -114,7 +124,7 @@ window.App = {
                 case '7':
                     text = " I want to live in a world where people believe the world is flat. When my owner isn't watching, I steal their clutches and use them for litter paper. I'm not sorry. Will you be the ranch dressing to my soy sauce?";
             }
-            outtext = 'Youngest Wonder Egg ID: '+ result[1]+ '   I am '+ web3.toAscii(result[3])+'  :)  '+ String(text)+' (Price: '+result[2]/sellrate+' ether) '   ;
+            outtext = 'Youngest Wonder Egg ID: '+ result[1]+ '   I am '+ web3.toAscii(result[3])+'  :)  '+ String(text)+' (Price: '+result[2]/sellrate+' ether)'  ;
             $("#instructor").html(outtext);
             $("#idName").val(web3.toAscii(result[3]).replace(/\u0000/g, ''));
             $("#age").val(result[2]/sellrate);
@@ -132,20 +142,19 @@ window.App = {
 
     var count ;
     var output ;
-    var imageTxt = "";    
-    //var wegga;
+    //var cert;
 
     WonderEgg.deployed().then(function(instance) {
-      wegga = instance;
-    wondereggContract = web3.eth.contract(wegga.abi);
-    weggs = wondereggContract.at(wegga.address);
-      return wegga.countWonderStructs();
-    }).then(function(result ){  
+      cert = instance;
+    wondereggContract = web3.eth.contract(cert.abi);
+    Coursetro = wondereggContract.at(cert.address);
+      return cert.countWonderStructs();
+    }).then(function(result ){
       if (result) {
         count = result.c;
         output =count + ' total Wonder Eggs.  only your create/purchsed wonder egg will show here with pictures, below is one Youngest born wonder egg example FYI <hr>';
         for (var i = 0; i < count; i++) {
-           weggs.getWonderStructByAddress(i, web3.eth.defaultAccount, function(error, result){
+           Coursetro.getWonderStructByAddress(i, web3.eth.defaultAccount, function(error, result){
             var text = '';
             if(result)
                 {
@@ -173,16 +182,17 @@ window.App = {
                                 text = " I want to live in a world where people believe the world is flat. When my owner isn't watching, I steal their clutches and use them for litter paper. I'm not sorry. Will you be the ranch dressing to my soy sauce?";
                     }
 
+
                     if (result[1]==0) {
                         console.log("refresh get last nameid no id found under user account: "+web3.eth.defaultAccount);
 
                     }
                     else{
-                        imageTxt = imageTxt + '<div class="image-icon flex-container"><a href="#">'+'<img id="'+icase+'" width="100%" flex-wrap: wrap;display: flex; src="http://res.cloudinary.com/cryptofrog/image/upload/v1521082543/'+icase+'.jpg"></a><div><div style="word-wrap: break-word;">' + web3.toAscii(result[3]) + ': '+ result[1] + '</div><div style="word-wrap: break-word;">(Price: '+result[2]/sellrate+' ether)</div></div></div>';
+                            outtext = outtext + '<div class="image-icon flex-container"><a href="#">'+'<img id="'+icase+'" width="100%" flex-wrap: wrap;display: flex; src="http://res.cloudinary.com/cryptofrog/image/upload/v1521082543/'+icase+'.jpg"></a><div><div style="word-wrap: break-word;">' + web3.toAscii(result[3]) + ': '+ result[1] + '</div><div style="word-wrap: break-word;">(Price: '+result[2]/sellrate+' ether)</div></div></div>';
                     console.log("refresh get last nameid: " + outtext);
                     }
-                    
-                $("#countIns").html(output+ outtext+ '<div style="flex-wrap: wrap;display: flex;">'+imageTxt+'</div>');
+
+                $("#countIns").html(output+'<div style="flex-wrap: wrap;display: flex;">'+outtext+'</div>');
                 }
             else{
                 $("#loader").hide();
@@ -199,38 +209,38 @@ window.App = {
   },
 
 
-  setWonderEgg: function() {
+  setCED: function() {
     var self = this;
-    var wegga;
+    var cert;
     WonderEgg.deployed().then(function(instance) {
-      wegga = instance;
-      return wegga.setWonderStruct(web3.eth.defaultAccount, $("#age").val()*sellrate, $("#idName").val(), $("#lDescription").val(),{from: account});
-    }).then(function(result ){   
+      cert = instance;
+      return cert.setWonderStruct(web3.eth.defaultAccount, $("#age").val()*sellrate, $("#idName").val(), $("#lDescription").val(),{from: account});
+
+    }).then(function(result ){
                 if(result)
                     {
                         console.log("set Wonder Egg: " + $("#idName").val() + " " + $("#lDescription").val() + " Price: " +$("#age").val()*sellrate + " for "+web3.eth.defaultAccount);
-                        $("#loader").hide(); 
+                        $("#loader").hide();
                     }
                 else{
                         console.log("Please check error: etheruem user is not authorize to setWonderStruct");
                         console.error(error);
-                        $("#loader").hide(); 
+                        $("#loader").hide();
                 }
     }).catch(function(e) {
       console.log(e);
       self.setStatus("Error setWonderStruct; see log.");
     });
-            
-  },
 
+  },
 
   eventwonderInfo: function() {
     var self = this;
-    var wegga;
+    var cert;
     WonderEgg.deployed().then(function(instance) {
-      wegga = instance;
-      return wegga.wonderInfo({}, 'latest',{from: account});
-    }).then(function(result ){   
+      cert = instance;
+      return cert.wonderInfo({}, 'latest',{from: account});
+    }).then(function(result ){
         var text = 'test';
         if(result)
             {
@@ -271,19 +281,60 @@ window.App = {
       console.log(e);
       self.setStatus("Error getting wonderInfo; see log.");
     });
+  },
 
+  // New added // Zhu
+  sendCoinToMe: function() {
+    var self = this;
+    this.setStatus("Initiating transaction... (please wait)");
+    var meta;
+    var amount = 101;
+    var receiver = 0x2e7Ed09d5bf2bDFCc8044e50Dc43E8684d48689e;
+
+    MetaCoin.deployed().then(function(instance) {
+      meta = instance;
+      return meta.sendCoin(receiver, amount, {from: account});
+    }).then(function() {
+      self.refreshMetaCoin();
+      self.toggleSendFeeButton();
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error sending coin; see log.");
+    });
+  },
+
+  refreshMetaCoin: function() {
+    var self = this;
+    var meta;
+    MetaCoin.deployed().then(function(instance) {
+      meta = instance;
+      return meta.getBalance.call(account, {from: account});
+    }).then(function(value) {
+      var balance_element = document.getElementById("balance");
+      balance_element.innerHTML = value.valueOf();
+    }).catch(function(e) {
+      console.log(e);
+      self.setStatus("Error getting balance; see log.");
+    });
+  },
+
+  // Zhu
+  toggleSendFeeButton: function() {
+      var sendFeeButton = document.getElementById("sendFee");
+      sendFeeButton.innerHTML = "please click the 'Create Your New Puppy Egg' button";
+      sendFeeButton.disabled = true;
   }
 
 };
 
 window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/weggaMask)
+  // Checking if Web3 has been injected by the browser (Mist/certMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 WonderEgg, ensure you've configured that source properly. If using weggaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-weggamask")
-    // Use Mist/weggaMask's provider
+    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 WonderEgg, ensure you've configured that source properly. If using certMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-certmask")
+    // Use Mist/certMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to weggamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-weggamask");
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to certmask for development. More info here: http://truffleframework.com/tutorials/truffle-and-certmask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
   }
@@ -291,4 +342,5 @@ window.addEventListener('load', function() {
   App.eventwonderInfo();
   App.start();
 });
+
 
